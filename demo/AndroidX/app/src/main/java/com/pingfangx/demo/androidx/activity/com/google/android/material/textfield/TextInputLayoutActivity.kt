562ed90f8ef928e04482b62d3435e519f83984bf
pingfangx <pingfangx@pingfangx.com>
2019-06-12 15:48:03 +0800

    [view]添加 TextInputLayoutActivity

---
+++ demo/AndroidX/app/src/main/java/com/pingfangx/demo/androidx/activity/com/google/android/material/textfield/TextInputLayoutActivity.kt
@@ -0,0 +1,10 @@
+package com.pingfangx.demo.androidx.activity.com.google.android.material.textfield
+
+import com.pingfangx.demo.androidx.base.BaseActivity
+
+/**
+ * @author pingfangx
+ * @date 2019/6/12
+ */
+class TextInputLayoutActivity : BaseActivity() {
+}
