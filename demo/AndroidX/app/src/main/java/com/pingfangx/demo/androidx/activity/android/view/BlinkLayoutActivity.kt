07e2ab35eb083f435e83a03a310f4633db1bc4dd
pingfangx <pingfangx@pingfangx.com>
2019-06-12 12:21:49 +0800

    [view]添加 BlinkLayout

---
+++ demo/AndroidX/app/src/main/java/com/pingfangx/demo/androidx/activity/android/view/BlinkLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.pingfangx.demo.androidx.activity.android.view
+
+import com.pingfangx.demo.androidx.base.BaseActivity
+
+/**
+ * Blink 示例
+ *
+ * @author pingfangx
+ * @date 2019/6/12
+ */
+class BlinkLayoutActivity : BaseActivity() {
+}
