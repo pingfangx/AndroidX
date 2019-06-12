9c8ee58f260c0adeac7676f9309bf0987002a3b2
pingfangx <pingfangx@pingfangx.com>
2019-06-12 14:54:06 +0800

    [view]添加 LinearLayout 测试 measureWithLargestChild

---
+++ demo/AndroidX/app/src/main/java/com/pingfangx/demo/androidx/activity/android/widget/LinearLayoutActivity.kt
@@ -0,0 +1,10 @@
+package com.pingfangx.demo.androidx.activity.android.widget
+
+import com.pingfangx.demo.androidx.base.BaseActivity
+
+/**
+ * @author pingfangx
+ * @date 2019/6/12
+ */
+class LinearLayoutActivity : BaseActivity() {
+}
