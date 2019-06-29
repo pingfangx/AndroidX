9fe751d488d514abfc1db741b989eaf432fb6790
pingfangx <pingfangx@pingfangx.com>
2019-06-29 12:20:21 +0800

    [love] 升级 1.0.1，增加可以保存自己的手势

--- demo/AndroidX/demo/love/src/main/java/com/pingfangx/demo/love/MainActivity.kt
+++ demo/AndroidX/demo/love/src/main/java/com/pingfangx/demo/love/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.pingfangx.demo.love
 
+import android.gesture.Gesture
 import android.gesture.GestureLibraries
 import android.gesture.GestureLibrary
 import android.gesture.GestureOverlayView
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import org.jetbrains.anko.defaultSharedPreferences
 import org.jetbrains.anko.toast
+import java.io.File
 
 /**
  * 学习手势的
  */
 class MainActivity : AppCompatActivity() {
     companion object {
-        const val MODE_SAVE = 1
+        const val MODE_DRAW = 1
         const val MODE_RECOGNIZE = 2
         const val TAG = "MainActivity"
         const val KEY_DRAW_TIMES = "draw_times"
@@ -23,37 +28,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var mode: Int = 2
-    private val name = "爱心"
+    private val loveGestureName by lazy {
+        getString(R.string.gesture_heart_gesture_name)
+    }
     private val sp by lazy {
         defaultSharedPreferences
     }
-    private var draw_times = 0
-    private var draw_love_times = 0
+    private var drawTimes = 0
+    private var drawLoveTimes = 0
     private val textView: TextView by lazy {
         findViewById<TextView>(R.id.textView)
     }
-    private val gestureLibrary: GestureLibrary by lazy {
-        val library = GestureLibraries.fromRawResource(this, R.raw.gestures)
-        library.load()
-        library
+    private val fileGestureLibrary: GestureLibrary by lazy {
+        GestureLibraries.fromFile(File(filesDir, "gestures"))
     }
+    private lateinit var gestureLibrary: GestureLibrary
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initGestureLibrary()
         initViews()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu?.add(0, 1, 0, getString(R.string.gesture_draw_your_heart))
+                ?.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        menu?.add(0, 2, 0, getString(R.string.gesture_toggle_mode, getString(R.string.gesture_mode_recognize)))
+                ?.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            1 -> {
+                toggleDrawMode()
+                return true
+            }
+            2 -> {
+                toggleRecognizeMode()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initGestureLibrary() {
+        if (fileGestureLibrary.load()) {
+            gestureLibrary = fileGestureLibrary
+        } else {
+            //默认是文件的，如果文件未加载，则加载默认的资源
+            gestureLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures)
+            gestureLibrary.load()
+        }
+    }
+
     private fun initViews() {
-        draw_times = sp.getInt(KEY_DRAW_TIMES, 0)
-        draw_love_times = sp.getInt(KEY_DRAW_LOVE_TIMES, 0)
+        drawTimes = sp.getInt(KEY_DRAW_TIMES, 0)
+        drawLoveTimes = sp.getInt(KEY_DRAW_LOVE_TIMES, 0)
         val gestureOverlayView = findViewById<GestureOverlayView>(R.id.gestureOverlayView)
         gestureOverlayView.addOnGesturePerformedListener { overlay, gesture ->
             when (mode) {
-                MODE_SAVE -> {
-                    overlay.context.toast("添加手势 [$name]")
-                    gestureLibrary.addGesture(name, gesture)
-                    gestureLibrary.save()
+                MODE_DRAW -> {
+                    showSaveDialog(gesture)
                 }
                 MODE_RECOGNIZE -> {
                     val recognize = gestureLibrary.recognize(gesture)
@@ -61,16 +99,16 @@ class MainActivity : AppCompatActivity() {
                     val max = recognize.maxBy { it.score }
                     if (max != null && max.score > 2) {
                         val text = when (max.name) {
-                            "爱心" -> when {
+                            loveGestureName -> when {
                                 max.score > 10 -> {
                                     //爱心
-                                    draw_love_times++
+                                    drawLoveTimes++
                                     when {
-                                        draw_love_times > 9 -> {
-                                            val n = Math.floor(Math.log10(draw_love_times.toDouble())).toInt()
-                                            getString(R.string.gesture_love_you_forever, draw_love_times, "永远".repeat(n))
+                                        drawLoveTimes > 9 -> {
+                                            val n = Math.floor(Math.log10(drawLoveTimes.toDouble())).toInt()
+                                            getString(R.string.gesture_love_you_forever, drawLoveTimes, "永远".repeat(n))
                                         }
-                                        draw_love_times > 1 -> getString(R.string.gesture_love_you_times, draw_love_times)
+                                        drawLoveTimes > 1 -> getString(R.string.gesture_love_you_times, drawLoveTimes)
                                         else -> getText(R.string.gesture_love_you)
                                     }
                                 }
@@ -88,13 +126,45 @@ class MainActivity : AppCompatActivity() {
                         textView.context.toast(text)
                     }
                     sp.edit()
-                            .putInt(KEY_DRAW_TIMES, ++draw_times)
-                            .putInt(KEY_DRAW_LOVE_TIMES, draw_love_times)
+                            .putInt(KEY_DRAW_TIMES, ++drawTimes)
+                            .putInt(KEY_DRAW_LOVE_TIMES, drawLoveTimes)
                             .apply()
-                    Log.d(TAG, "draw times $draw_times")
+                    Log.d(TAG, "draw times $drawTimes")
                 }
-                else -> overlay.context.toast("手势绘制完成")
+                else -> overlay.context.toast(R.string.gesture_toast_draw_finish)
             }
         }
+    }
+
+    private fun showSaveDialog(gesture: Gesture) {
+        val dialog = AlertDialog.Builder(this)
+                .setMessage(R.string.gesture_dialog_save_gesture_message)
+                .setPositiveButton(R.string.gesture_dialog_save_gesture_save) { _, _ ->
+                    saveGesture(gesture)
+                    toggleRecognizeMode()
+                }
+                .setNegativeButton(R.string.gesture_dialog_save_gesture_cancel, null)
+                .create()
+        dialog.show()
+    }
+
+    private fun saveGesture(gesture: Gesture) {
+        toast(getString(R.string.gesture_toast_save_gesture_success, loveGestureName))
+        fileGestureLibrary.addGesture(loveGestureName, gesture)
+        fileGestureLibrary.save()
+        //文件手势库才可以保存，赋值
+        gestureLibrary = fileGestureLibrary
+    }
+
+    private fun toggleDrawMode() {
+        mode = MODE_DRAW
+        toast(getString(R.string.gesture_toggle_mode, getString(R.string.gesture_mode_draw)))
+        textView.setText(R.string.gesture_draw_your_heart)
+    }
+
+    private fun toggleRecognizeMode() {
+        mode = MODE_RECOGNIZE
+        toast(getString(R.string.gesture_toggle_mode, getString(R.string.gesture_mode_recognize)))
+        textView.setText(R.string.gesture_please_draw_a_heart)
     }
 }
